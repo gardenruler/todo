8363ad6c602030d0c50b5fdc477257c15751cbfa
onethegarden <onethegarden@gmail.com>
2021-08-14 19:19:25 +0900

    todo input submit

--- frontend/src/todo/TodoInput.test.tsx
+++ frontend/src/todo/TodoInput.test.tsx
@@ -3,13 +3,16 @@ import { fireEvent, getByText, render } from '@testing-library/react';
 import TodoInput from './TodoInput';
 
 describe('<TodoForm/>', () => {
+  const onInsert = jest.fn();
   it('has input and a button', () => {
-    const { getByPlaceholderText, getByText } = render(<TodoInput />);
+    const { getByPlaceholderText, getByText } = render(
+      <TodoInput onInsert={onInsert} />,
+    );
     getByPlaceholderText('Add a task');
     getByText('submit');
   });
   it('change input', () => {
-    const { getByPlaceholderText } = render(<TodoInput />);
+    const { getByPlaceholderText } = render(<TodoInput onInsert={onInsert} />);
     const input = getByPlaceholderText('Add a task');
     fireEvent.change(input, {
       target: {
@@ -17,5 +20,21 @@ describe('<TodoForm/>', () => {
       },
     });
     expect(input).toHaveAttribute('value', 'TDD training');
+  });
+
+  it('submit and clears input', () => {
+    const { getByPlaceholderText, getByText } = render(
+      <TodoInput onInsert={onInsert} />,
+    );
+    const input = getByPlaceholderText('Add a task');
+    const button = getByText('submit');
+    fireEvent.change(input, {
+      target: {
+        value: 'TDD training',
+      },
+    });
+    fireEvent.click(button);
+    expect(onInsert).toBeCalledWith('TDD training');
+    expect(input).toHaveAttribute('value', '');
   });
 });
