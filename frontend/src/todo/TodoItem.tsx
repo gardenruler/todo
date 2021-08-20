d5d6fc4b368746ae8f891b32d264afc24dd2a2fc
onethegarden <onethegarden@gmail.com>
2021-08-20 21:12:53 +0900

    todo edit style

--- frontend/src/todo/TodoItem.tsx
+++ frontend/src/todo/TodoItem.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
+import { BsPencil } from 'react-icons/bs';
 import { Todo } from '../api/todoApi';
 
 export interface TodoProps {
@@ -8,28 +9,39 @@ export interface TodoProps {
 }
 const TodoItem = ({ todo, onToggle, onRemove }: TodoProps): JSX.Element => {
   const { id, content, done } = todo;
+  const [editable, setEditable] = useState(false);
   const toggle = useCallback(() => onToggle(id), [id, onToggle]);
   const remove = useCallback(() => onRemove(id), [id, onRemove]);
   return (
-    <li className={done ? 'done' : ''}>
-      <button className="toggle" type="button">
-        {' '}
-      </button>
-      <span
-        role="button"
-        tabIndex={0}
-        style={{
-          textDecoration: done ? 'line-through' : 'none',
-        }}
-        onClick={toggle}
-        onKeyDown={toggle}
-      >
-        {content}
-      </span>
-      <button className="delete" type="button" onClick={remove}>
-        ×
-      </button>
-    </li>
+    <>
+      <li className={done ? 'done' : ''}>
+        <button className="toggle" type="button" onClick={toggle}>
+          {' '}
+        </button>
+        <span
+          role="button"
+          tabIndex={0}
+          style={{
+            textDecoration: done ? 'line-through' : 'none',
+          }}
+          onKeyDown={toggle}
+        >
+          {editable ? <input type="text" value={content} /> : content}
+        </span>
+        <button
+          className="editable"
+          type="button"
+          onClick={() => {
+            setEditable(!editable);
+          }}
+        >
+          <BsPencil />
+        </button>
+        <button className="delete" type="button" onClick={remove}>
+          ×
+        </button>
+      </li>
+    </>
   );
 };
 
